a4e9f568503b01b3dc190a9707a9b8b124d815e9
Nilan1 <kolay16@qip.ru>
2016-11-25 22:14:49 +0200

    Add files via upload

---
+++ dz.js
@@ -0,0 +1,15 @@
+var name = "My name Nikolay";
+alert(name);  
+var	lastName = "lastname Poberezniy";
+alert(lastName);
+var birthday = ('birthday 20.11.88');
+alert(birthday);
+var programmingskills = "programmingskills initial";
+alert(programmingskills);
+var friend = "my friend is a programmer";
+alert(friend);
+
+console.log(name,lastName);
+console.log(birthday);
+console.log(programmingskills);
+console.log(friend);
